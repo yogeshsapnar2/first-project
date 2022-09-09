ef2f0c184000ddb1411b6651a43e61f57be77ad4
yogeshsapnar <110964746+yogeshsapnar@users.noreply.github.com>
2022-09-09 21:24:17 +0530

    rev0

---
+++ src/mypack/helloworld.java
@@ -0,0 +1,11 @@
+package mypack;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("HELLO WORLD");
+  System.out.println("This is my First Project");
+	}
+
+}
